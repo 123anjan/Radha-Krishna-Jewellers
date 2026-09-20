9202941c14c8cda0ac6c9364f71b5356ad3b6d75
Anjan Basak <115646260+123anjan@users.noreply.github.com>
2026-09-20 11:24:56 +0530

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"], // Add paths to all of your template files
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
