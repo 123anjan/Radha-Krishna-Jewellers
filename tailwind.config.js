a9a7d4a32c18151693cc57dfb0d3a3d7d356c8be
Anjan Basak <115646260+123anjan@users.noreply.github.com>
2026-09-20 10:54:31 +0530

    Add initial Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Add paths to all of your template files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
